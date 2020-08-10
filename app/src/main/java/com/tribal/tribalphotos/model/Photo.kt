bed83de441855b5061ea663a5311b15e741227b5
Henry De la Mano <hdelamano@gmail.com>
2020-08-10 04:50:16 -0400

    adding firebase analytics and crashlytics

--- app/src/main/java/com/tribal/tribalphotos/model/Photo.kt
+++ app/src/main/java/com/tribal/tribalphotos/model/Photo.kt
@@ -1,7 +1,11 @@
 package com.tribal.tribalphotos.model
 
 data class Photo (
+    var likes: Int = 0,
     val author: String? = null,
-    val name: String? = null,
-    val photos: List<String>?
+    val urlPhoto: String? = null,
+    val urlProfle: String? = null,
+    val descripImg: String? = null,
+    val descripProfile: String? = null
+
 )
